1c2e23a2014bc5afb70d01c3cc3677bcf0ef26ec
unknown <likhith2901@gmail.com>
2020-08-04 13:21:05 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import './App.css';
-import Data from './person/people
+import Data from './person/people';
 import styled from 'styled-components';
 
 const StyledButton=styled.button`
